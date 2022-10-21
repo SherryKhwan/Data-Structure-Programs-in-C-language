064f4846c943af68c582ce99c3515a79384ae326
SherryKhwan <110398254+SherryKhwan@users.noreply.github.com>
2022-10-21 12:49:41 +0500

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-// Bubble Sorting Algorithm:1
+
+// Bubble Sorting Algorithm:
+// In bubble sorting, we will compare 2 values and if the next val is greater than our current value we will swap it right away and after the first full iteration the biggest number will be at the last position.
+
+// cur = current, next = next value
+
 int main() {
-  int c, f = 0;
-  int j = f + 1;
+  int cur = 0;
+  int next = cur + 1;
   int o = 9;
-  int l = 0;
-  int m;
-  int k = 0;
+  int temp = 0;
   int data[10]= {9, 2, 3, 22, 1, 12, 15, 56, 33, 11};
   for (int n = 0; n < 9; n++){
     for (int i =  0; i < o; i++){
-      if (data[f] > data[j] ){  
-        k = data[j];
-        data[j] = data[f];
-        data[f] = k;
+      if (data[cur] > data[next] ){  
+        temp = data[next];
+        data[next] = data[cur];
+        data[cur] = temp;
       }
-        f++;                   
-        j++;                    
+        cur++;                   
+        next++;                    
     }
-    f = 0;
-    j = f + 1;
-    o = o -1;
+    cur = 0;
+    next = cur + 1;
+    o = o - 1;
   }
     for (int m = 0; m < 10; m++){
     printf(" %d,", data[m]);
